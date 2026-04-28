4b11b654fb5b0ed525065b0d6054d7fc15e0a024
Vinícius Gabriel Inacio <164924053+Inacio727@users.noreply.github.com>
2026-04-28 03:08:52 -0300

    Update PerfilPage.vue

---
+++ src/views/PerfilPage.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { IonContent, IonInput, IonPage } from '@ionic/vue';
+import { ref, watch } from 'vue';
+
+const nome = ref('');
+
+watch(nome, (novo) => {
+    console.log('Nome alterado:', novo);
+});
+</script>
+
+<template>
+    <IonPage>
+        <IonContent class="ion-padding">
+            <IonInput v-model="nome" placeholder="Seu nome"></IonInput>
+        </IonContent>
+    </IonPage>
+</template>
