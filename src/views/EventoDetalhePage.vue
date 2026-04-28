1c1438e617ef8e8e7add52a0f5dbe8effd024cae
Vinícius Gabriel Inacio <164924053+Inacio727@users.noreply.github.com>
2026-04-28 03:04:54 -0300

    favoritosPage, mas nao ta linkado com nada

---
+++ src/views/EventoDetalhePage.vue
@@ -0,0 +1,35 @@
+<script setup>
+import { useRoute } from 'vue-router';
+import { eventos } from '@/data/eventos';
+import { computed } from 'vue';
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle, IonContent, IonPage } from '@ionic/vue';
+
+const route = useRoute();
+
+const evento = computed(() =>
+    eventos.find(e => e.id == route.params.id)
+);
+
+function toggleFavorito() {
+    evento.value.favorito = !evento.value.favorito;
+}
+</script>
+
+<template>
+    <IonPage>
+        <IonContent class="ion-padding">
+            <IonCard v-if="evento">
+                <IonCardHeader>
+                    <IonCardTitle>{{ evento.titulo }}</IonCardTitle>
+                </IonCardHeader>
+
+                <IonCardContent>
+                    {{ evento.descricao }}
+                    <IonButton @click="toggleFavorito">
+                        Favoritar
+                    </IonButton>
+                </IonCardContent>
+            </IonCard>
+        </IonContent>
+    </IonPage>
+</template>
---
+++ src/views/FavoritosPage.vue
@@ -0,0 +1,26 @@
+<script setup>
+
+import { eventos } from '@/data/eventos';
+import { IonContent, IonItem, IonList, IonPage } from '@ionic/vue';
+import { computed } from 'vue';
+
+const favoritos = computed(() =>
+    eventos.filter(e => e.favorito)
+);
+</script>
+
+<template>
+    <IonPage>
+        <IonContent class="ion-padding">
+            <div v-if="favoritos.length === 0">
+                Nenhum favorito ainda
+            </div>
+
+            <IonList v-else>
+                <IonItem v-for="x in favoritos" :key="x.id">
+                    {{ e.titulo }}
+                </IonItem>
+            </IonList>
+        </IonContent>
+    </IonPage>
+</template>
