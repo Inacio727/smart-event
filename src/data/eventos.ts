c8415c6aa9d908cb0d75f3145d11625fb8b65163
Vinícius Gabriel Inacio <164924053+Inacio727@users.noreply.github.com>
2026-04-28 02:33:28 -0300

    Create eventos.ts

---
+++ src/data/eventos.ts
@@ -0,0 +1,14 @@
+export const eventos = [
+    {
+        id: 1,
+        titulo: "Show do Mau Contato",
+        descricao: "Banda iniciante ao vivo",
+        favorito: false
+    },
+    {
+        id: 2,
+        titulo: "Workshop Senac",
+        descricao: "Aprenda Vue",
+        favorito: false
+    }
+];
