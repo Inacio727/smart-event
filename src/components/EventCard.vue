75bbe12344d4991f5fda7f1f23934321acc78c74
Vinícius Gabriel Inacio <164924053+Inacio727@users.noreply.github.com>
2026-04-28 03:21:42 -0300

    component eventCard

---
+++ src/components/EventCard.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { IonButton, IonCard, IonCardContent, IonCardHeader, IonCardTitle } from '@ionic/vue';
+
+defineProps(['evento']);
+defineEmits(['favoritar']);
+</script>
+
+<template>
+    <IonCard>
+        <IonCardHeader>
+            <IonCardTitle>{{ evento.titulo }}</IonCardTitle>
+        </IonCardHeader>
+
+        <IonCardContent>
+            <IonButton @click="$emit('favoritar')">
+                Favoritar
+            </IonButton>
+        </IonCardContent>
+    </IonCard>
+</template>
---
+++ src/views/PerfilPage.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { ref, watch } from 'vue';
+
+const nome = ref('');
+
+watch(nome, (novo) => {
+  console.log('Nome alterado:', novo);
+});
+</script>
+
+<template>
+  <ion-page>
+    <ion-content class="ion-padding">
+      <ion-input v-model="nome" placeholder="Seu nome"></ion-input>
+    </ion-content>
+  </ion-page>
+</template>
